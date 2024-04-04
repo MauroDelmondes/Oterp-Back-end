5c6dadd586ab07a85d1eafb1b488f6bbb6ee857a
Mauro <mhdelmonde@gmail.com>
2024-04-04 19:59:31 -0400

    deleting weatherforecast files

--- obj/Debug/net8.0/Oterp-Back-end.AssemblyInfo.cs
+++ obj/Debug/net8.0/Oterp-Back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Oterp-Back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5499b904c4deab960402b250ea9e33081c1bae8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6faf8e826f0693818cace0e426e8c4c74da9741b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Oterp-Back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Oterp-Back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
